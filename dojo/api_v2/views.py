8a346e3c5d931a96b08783c25ed0639d58539618
Santiago Gaviria Oliveros <sagavir@bancolombia.com.co>
2026-08-05 13:01:41 -0500

    feat(tests): conditionally propagate engagement changes to findings based on settings

--- dojo/api_v2/views.py
+++ dojo/api_v2/views.py
@@ -2410,10 +2410,13 @@ class TestsViewSet(
         previous_engagement_id = serializer.instance.engagement_id
 
         updated_test = serializer.save()
+        enable_service_propagation = GeneralSettings.get_value(
+            "ENABLE_PROPAGATE_TEST_ENGAGEMENT_TO_FINDING_SERVICE", True,
+        )
 
         # If the engagement changed via this API update, propagate the new
         # engagement name to the `service` field of all Findings on this Test.
-        if previous_engagement_id != updated_test.engagement_id:
+        if enable_service_propagation and previous_engagement_id != updated_test.engagement_id:
             try:
                 new_service_value = updated_test.engagement.name if updated_test.engagement else None
                 if new_service_value is not None:
