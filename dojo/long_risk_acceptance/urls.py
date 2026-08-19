badef4263696e079aa32bec08f17af55f2a2753f
Rene Jalvin <bjalvin@bancolombia.com.co>
2026-08-19 06:40:09 -0500

    feat: new views risk acceptance all

--- dojo/long_risk_acceptance/urls.py
+++ dojo/long_risk_acceptance/urls.py
@@ -6,5 +6,10 @@ urlpatterns = [
         "long_risk_acceptance/details/<int:pk>/",
         views.view_long_risk_acceptance_details,
         name='view_long_risk_acceptance_details'
+    ),
+    path(
+        "long_risk_acceptance/list/",
+        views.view_long_risk_acceptance_list,
+        name='view_long_risk_acceptance_list'
     )
 ]
